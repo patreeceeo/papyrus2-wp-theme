4c256f1efbc66af0929ad8339412ce41acedf16d
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 14:04:13 -0800

    add init function to clean up output of wp_head

--- papyrus2/functions.php
+++ papyrus2/functions.php
@@ -5,6 +5,23 @@
  * @package Papyrus2
  */
 
+function papyrus2_head_cleanup() {
+  remove_action('wp_head', 'feed_links', 2);
+  remove_action('wp_head', 'feed_links_extra', 3);
+  remove_action('wp_head', 'rsd_link');
+  remove_action('wp_head', 'wlwmanifest_link');
+  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+  remove_action('wp_head', 'wp_generator');
+  remove_action('wp_head', 'wp_shortlink_wp_head', 10, 0);
+
+  global $wp_widget_factory;
+  remove_action('wp_head', array($wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style'));
+
+  add_filter('use_default_gallery_style', '__return_null');
+}
+add_action('init', 'papyrus2_head_cleanup');
+
+
 /**
  * Set the content width based on the theme's design and stylesheet.
  */
