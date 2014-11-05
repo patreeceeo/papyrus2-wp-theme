4354e1bd952e1c54af0f7998e26be9b98886a296
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 10:53:25 -0800

    update header

--- papyrus2/header.php
+++ papyrus2/header.php
@@ -19,19 +19,13 @@
 </head>
 
 <body <?php body_class(); ?>>
-<div id="page" class="hfeed site">
-	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'papyrus2' ); ?></a>
-
-	<header id="masthead" class="site-header" role="banner">
-		<div class="site-branding">
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</div>
-
-		<nav id="site-navigation" class="main-navigation" role="navigation">
-			<button class="menu-toggle"><?php _e( 'Primary Menu', 'papyrus2' ); ?></button>
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); ?>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
+  <nav class="p-layout-top-nav">
+    <ul>
+      <li>
+        <a href="/portfolio">Portfolio</a>
+      </li>
+      <li class="u-pull-right">
+        <a href="/portfolio">Blog</a>
+      </li>
+    </ul> 
+  </nav>
--- papyrus2/page-landing.php
+++ papyrus2/page-landing.php
@@ -8,14 +8,4 @@ get_header(); ?>
 
 
 <main class="p-layout-full-screen p-bg-solid-dark">
-  <nav class="p-layout-top-nav">
-    <ul>
-      <li>
-        <a href="/portfolio">Portfolio</a>
-      </li>
-      <li class="u-pull-right">
-        <a href="/portfolio">Blog</a>
-      </li>
-    </ul> 
-  </nav>
 </main>
