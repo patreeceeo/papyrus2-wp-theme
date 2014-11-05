80392755bdf19c996ecbacc8b765ae12b277cd69
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 10:42:03 -0800

    start creating landing page

--- papyrus2/page-landing.php
+++ papyrus2/page-landing.php
@@ -5,4 +5,27 @@
  * @package Papyrus2
  */
 ?>
-<h1>hello!</h1>
+
+
+<!DOCTYPE HTML>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Papyrus</title>
+
+  <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="favicon.gif">
+</head>
+<body class="p-bg-solid-dark">
+  <nav class="p-layout-top-nav">
+    <ul>
+      <li>
+        <a href="/portfolio">Portfolio</a>
+      </li>
+      <li class="u-pull-right">
+        <a href="/portfolio">Blog</a>
+      </li>
+    </ul> 
+  </nav>
+</body>
+</html>
