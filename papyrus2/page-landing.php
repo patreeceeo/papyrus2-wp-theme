54b644120ffd8ed1c6b3e471b1187feaf6223a97
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 18:28:23 -0800

    create welcome sign

--- papyrus2/page-landing.php
+++ papyrus2/page-landing.php
@@ -10,7 +10,13 @@ get_header(); ?>
 <main class="p-layout-full-screen p-bg-solid-dark u-zi-bg js-smart-scrollable">
   <div class="p-welcome-sign">
     <div class="p-welcome-sign-inner">
-      <img src="<?php echo get_template_directory_uri() . "/assets/papyrus-logo.svg"; ?>">
+      <figure>
+        <img src="<?php echo get_template_directory_uri() . "/assets/papyrus-logo.svg"; ?>">
+      </figure><section>
+        <h1>Patrick Canfield</h1>
+        <h3>Software Engineer &amp;&amp;<br>Designer for Freelance</h3>
+        <p>Need an expert for your next project? I delight in delivering <a href="/blog/tag/balanced">balanced</a> solutions, exceeding expectations and sharing my knowledge.</p>
+      </section>
     </div>
   </div>
 </main>
