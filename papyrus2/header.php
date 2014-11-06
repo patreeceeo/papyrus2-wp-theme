f40f56cff6dfe427074d020d8376f4adfb04e39a
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 21:21:56 -0800

    fix meta tag

--- papyrus2/header.php
+++ papyrus2/header.php
@@ -16,7 +16,7 @@
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="icon" href="<?php echo get_template_directory_uri() . "/favicon.gif"; ?>">
 
-<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=false">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 
 <?php wp_head(); ?>
 </head>
