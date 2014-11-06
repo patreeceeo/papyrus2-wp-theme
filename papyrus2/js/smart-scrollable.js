d6bcbbd7e8f77002bc530eeaae2003405254a5bf
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-05 20:44:10 -0800

    make it responsive

--- papyrus2/js/smart-scrollable.js
+++ papyrus2/js/smart-scrollable.js
@@ -1,10 +1,18 @@
 !(function ($) {
   "use strict";
   $(function () {
-    $(".js-smart-scrollable").each(function () {
-      if($(window).innerHeight() >= $(this).innerHeight() || $(this).css("position") !== "static") {
-        $("html").css("overflow-y", "hidden");
-      }
-    });
+    function refresh () {
+      $(".js-smart-scrollable").each(function () {
+        if($(window).innerHeight() >= $(this).outerHeight(true)) {
+          $(this).parent().css("overflow-y", "hidden");
+        } else {
+          $(this).parent().css("overflow-y", "scroll");
+        }
+      });
+    }
+
+    $(window).resize(refresh);
+
+    refresh();
   });
 })(this.$);
--- papyrus2/page-landing.php
+++ papyrus2/page-landing.php
@@ -7,14 +7,14 @@
 get_header(); ?>
 
 
-<main class="p-layout-full-screen p-bg-solid-dark u-zi-bg js-smart-scrollable">
+<main class="p-layout-full-screen p-bg-solid-dark u-zi-bg">
   <div class="p-welcome-sign">
-    <div class="p-welcome-sign-inner">
+    <div class="p-welcome-sign-inner js-smart-scrollable">
       <figure>
         <img src="<?php echo get_template_directory_uri() . "/assets/papyrus-logo.svg"; ?>">
       </figure><section>
         <h1>Patrick Canfield</h1>
-        <h3>Software Engineer &amp;&amp;<br>Designer for Freelance</h3>
+        <h3>Software Engineer &amp;&amp; Designer for Freelance</h3>
         <p>Need an expert for your next project? I delight in delivering <a href="/blog/tag/balanced">balanced</a> solutions, exceeding expectations and sharing my knowledge.</p>
       </section>
     </div>
