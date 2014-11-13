bfa4c88ebfdda317fbdc887de3452287e47997b2
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-13 08:12:21 -0800

    clear out footer

--- papyrus2/footer.php
+++ papyrus2/footer.php
@@ -12,9 +12,6 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'papyrus2' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'papyrus2' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'papyrus2' ), 'Papyrus2', '<a href="http://underscores.me/" rel="designer">Underscores.me</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
