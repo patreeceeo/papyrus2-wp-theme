35341f0eeb16e720ecae38b3586a7f7dbc85df4f
Patrick Canfield <patrickcandoit@gmail.com>
2014-11-17 17:17:00 -0800

    fix bug in $.fn.fitText
    When the text element to be dynamically sized was directly inside a block
    element with an automatically determined width, e.g. a div with no matching CSS
    width rule or in-line style property, fitText would still use the current width
    of the parent as the width to fit the text into. In effect, the text
    would shrink with the parent when the window became narrower than the parent
    element, but would not grow again because the parent did not grow. The
    solution, for now, is to only consider the width of the parent if it is
    set in-line. Otherwise just use the window width.

--- papyrus2/js/papyrus-main.js
+++ papyrus2/js/papyrus-main.js
@@ -52,26 +52,48 @@ $(function () {
   showPageForHash();
 
   $.fn.fitText = function (options) {
-    var parentWidth, newFontSize, ratio, maxSize;
+    var parent, 
+        parentWidth, 
+        newFontSize, 
+        formerFontSize, 
+        ratio, 
+        maxSize;
+
+    function cssInt (element, propName) {
+      return parseInt($(element).css(propName)) || 0;
+    }
+
     options = options || {};
     $(this).each(function () {
       maxSize = parseInt(options.maxSize) || Number.POSITIVE_INFINITY; 
+      formerFontSize = cssInt(this, "font-size");
      
       if(options.maxSize === "initial") {
         if($(this).data("fit-text-max-size") == null) {
-          maxSize = parseInt($(this).css("font-size")) || 0;
+          maxSize = formerFontSize;
           $(this).data("fit-text-max-size", maxSize);
         } else {
           maxSize = $(this).data("fit-text-max-size");
         }
       }
 
+
+      $(this).css({
+        "font-size": "",
+        "line-height": ""
+      });
+
+      parent = $(this).parent().get(0);
+
+
       parentWidth = Math.min(
-        $(this).parent().innerWidth() -
-          (parseInt($(this).parent().css("padding-left")) || 0) -
-          (parseInt($(this).parent().css("padding-right")) || 0), 
-        screen.width
-      );
+        ($(parent).innerWidth() || Number.POSITIVE_INFINITY), 
+        $(window).width()
+        ) - cssInt(parent, "padding-left") - 
+            cssInt(parent, "padding-right") - 
+            cssInt(this, "padding-left") -
+            cssInt(this, "padding-right");
+
       if(parentWidth !== $(this).data("fit-text-parent-former-width")) {
         $(this).css({
           "position": "absolute"
@@ -79,15 +101,18 @@ $(function () {
         ratio = parentWidth / $(this).innerWidth();
         newFontSize = Math.min(
             maxSize, 
-            Math.floor((parseInt($(this).css("font-size")) || 0) * ratio * 1)
+            Math.floor(cssInt(this, "font-size") * ratio * 1)
         );
-        $(this).css({
-          "position": "static",
-          "font-size": newFontSize + "px",
-          "line-height": newFontSize + "px"
-        });
         $(this).data("fit-text-parent-former-width", parentWidth);
+      } else {
+        newFontSize = formerFontSize;
       }
+
+      $(this).css({
+        "position": "static",
+        "font-size": newFontSize + "px",
+        "line-height": newFontSize + "px"
+      });
     });
   };
 
